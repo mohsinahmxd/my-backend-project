1799a903e49cf176728a3a8bffc60543e5b6810e
Mohsin <mohsin-ahmed01@outlook.com>
2024-01-19 10:35:34 +0000

    this commit adjusts getArticlesViaId endpoint tests in response to tutor feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,7 +6,6 @@ const testData = require("../db/data/test-data/index.js")
 const allEndpoints = require("../endpoints.json")
 
 
-
 beforeEach(() => {
     return seed(testData);
 })
@@ -56,6 +55,24 @@ describe('testing GET /api/articles/:article_id', () => {
         .expect(200);
     });
     test('should respond with an article object based on the given id', () => { 
+        const expected = {article : [{   
+            article_id: 2,
+            title: "Sony Vaio; or, The Laptop",
+            topic: "mitch",
+            author: "icellusedkars",
+            body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+            created_at: "2020-10-16T05:03:00.000Z",
+            votes: 0,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+          }]}
+
+        return request(app).get("/api/articles/2")
+        .then(data => {
+            expect(data._body).toMatchObject(expected);
+        })
+    });
+    test('should respond with an article object based on the given id AND this object must contain the comment count too for that article', () => { 
         const expected = {article : [{   
             article_id: 2,
             title: "Sony Vaio; or, The Laptop",
--- errors/index.js
+++ errors/index.js
@@ -22,6 +22,5 @@ exports.handlePsqlErrors = (err, req, res, next) => {
 
 // final err
 exports.handleInternalServerError = (err, req, res, next) => {
-    console.log(err);
     res.status(500).send({ msg: 'Internal Server Error' });
 }
