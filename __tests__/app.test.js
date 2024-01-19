09d7dd94cf16282a70c7faf4f4e91b0bf8c82803
Mohsin <mohsin-ahmed01@outlook.com>
2024-01-19 16:38:11 +0000

    move expected inside test block

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -550,14 +550,14 @@ describe('test GET /api/users', () => {
 });
 
 describe('testing GET /api/users/:username', () => {
-    const expected = {user : [{
+    test('should respind with a status code 200 and a username obj based on the given id', () => {
+        const expected = {user : [{
             username: 'lurker',
             name: 'do_nothing',
             avatar_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png'
         }]
-    }
+    }   
 
-    test('should respind with a status code 200 and a username obj based on the given id', () => {
         return request(app).get("/api/users/lurker")
         .expect(200)
         .then(data => {
