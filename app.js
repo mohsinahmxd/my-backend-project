4d141daf4bced2dc2224396db6e04eb59c51eabe
Mohsin <mohsin-ahmed01@outlook.com>
2024-02-05 10:09:52 +0000

    Enable CORS on BE repo

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const {handleCustomError, handlePsqlErrors, handleInternalServerError} = require("./errors/index.js")
+const {
+  handleCustomError,
+  handlePsqlErrors,
+  handleInternalServerError,
+} = require("./errors/index.js");
 const apiRouter = require("./routes/api-router.js");
+const cors = require("cors");
 
 // routing / middleware
+app.use(cors());
 app.use(express.json());
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
 // errors
-app.use(handleCustomError)
-app.use(handlePsqlErrors)
-app.use(handleInternalServerError)
+app.use(handleCustomError);
+app.use(handlePsqlErrors);
+app.use(handleInternalServerError);
 
 module.exports = app;
